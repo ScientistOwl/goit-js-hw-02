c17abc5819791a5998091a38527f727ab12f20e3
ScientistOWl <BlackStoneGame22@gmail.com>
2024-12-06 08:43:57 +0200

    hw2

--- js/task-2.js
+++ js/task-2.js
@@ -6,9 +6,15 @@ function formatMessage(message, maxLength) {
   }
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+console.log("task 2", formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log("task 2", formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log("task 2", formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log("task 2", formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(
+  "task 2",
+  formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)
+); // "Nunc sed turpis..."
+console.log(
+  "task 2",
+  formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)
+); // "Nunc sed turpis a felis in nunc fringilla"
--- js/task-3.js
+++ js/task-3.js
@@ -5,10 +5,13 @@ function checkForSpam(message) {
   );
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log("task 3", checkForSpam("Latest technology news")); // false
+console.log("task 3", checkForSpam("JavaScript weekly newsletter")); // false
+console.log("task 3", checkForSpam("Get best sale offers now!")); // true
+console.log("task 3", checkForSpam("Amazing SalE, only tonight!")); // true
+console.log("task 3", checkForSpam("Trust me, this is not a spam message")); // true
+console.log(
+  "task 3",
+  checkForSpam("Get rid of sPaM emails. Our book in on sale!")
+); // true
+console.log("task 3", checkForSpam("[SPAM] How to earn fast money?")); // true
